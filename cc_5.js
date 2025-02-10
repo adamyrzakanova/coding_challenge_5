57d5b853acd454fbde9a693bd988e2d58398b4fd
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-02-09 20:54:20 -0600

    Task 1 - Customer Profile.

---
+++ cc_5.js
@@ -0,0 +1,5 @@
+// Task 1: Object Properties - Customer Profile
+const customer = { name: "John Doe", age: 35, email: "john.doe@email.com" };
+console.log(`Customer Name: ${customer.name}`);
+console.log(`Customer Age: ${customer.age}`);
+console.log(`Customer Email: ${customer.email}`);
